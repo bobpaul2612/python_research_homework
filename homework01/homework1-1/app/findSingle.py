2e062c57005f7fc87f15938dc58d6ada400404c6
AlexYang <killerlightning@gmail.com>
2020-03-05 20:11:26 +0800

    finish01

--- homework01/homework1-1/app/findSingle.py
+++ homework01/homework1-1/app/findSingle.py
@@ -22,21 +22,50 @@ def _getInput():
             
 
 
-def _findSingle():
+def _findSingle(inputString):
     '''
     The findSingle function will find the single charector
     by the User input
-    '''
-    inputString = _getInput()
-    print(inputString)
 
+    return the single charector
+    '''
+    mark = [0 for i in range(len(inputString))]
+    ansIndex = -1
+
+    for i in range(len(inputString)):
+        if mark[i] == 1:
+            continue
+        for j in range(i+1 , len(inputString)):
+            if mark[j] == 1:
+                continue
+            if(inputString[i] == inputString[j]):
+                mark[i] = mark[j] = 1
+    
+    for i in range(len(mark)):
+        if mark[i] == 0:
+            ansIndex = i
+
+    if ansIndex != -1:
+        return inputString[ansIndex]
+    return 'This input no answer !'
 
 def main():
     '''
     The main function will execute the findSingle() 
     to find the single charector (only one)
     '''
-    _findSingle()
+    while True:
+        try:
+            inputString = _getInput()
+        except:
+            sys.exit(-1)
+    
+        ans = _findSingle(inputString)
+
+        if len(ans) == 1:
+            print('The single charector is : ' , ans)
+        else:
+            print('This input no answer !')
 
 if __name__ == "__main__":
     main()
